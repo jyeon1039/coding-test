4e2794ceb23e29d40d05f1736e9cc8289047be55
jyeon1039 <69183944+jyeon1039@users.noreply.github.com>
2022-06-02 23:23:17 +0900

    [Silver IV] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split())) 
+res = 0 
+
+for i in arr:
+    cnt = 0
+    
+    if(i == 1):
+        continue
+    
+    for j in range(2, i+1):
+        if(i % j == 0):
+            cnt += 1
+            
+    if(cnt == 1):
+        res += 1
+print(res)

3794dbae10bd1e9893ef98a73faf5df78f8ded96
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2022-07-03 11:44:12 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,4 +1,4 @@
-def getAnswer(place):
+def isCorrect(place):
     for y in range(5):
         for x in range(5):
             if place[y][x] != 'P':
@@ -9,41 +9,34 @@ def getAnswer(place):
                 return 0
             if 0 < x < 4 and (place[y][x+1] == 'P' or place[y][x-1] == 'P'):
                 return 0
-
-            # 거리가 2인 곳에 응시자가 있는 경우
-            if 0 <= y+1 < 5 and place[y+1][x] == 'O':
-                if 0 <= y+2 < 5 and place[y+2][x] == 'P':
+            
+            # 거리가 2인 곳에 응시자가 있는 경우 - 직선
+            if x < 3:
+                if place[y][x+1] == 'O' and place[y][x+2] == 'P':
                     return 0
-                if 0 <= x+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
+            if y < 3:
+                if place[y+1][x] == 'O' and place[y+2][x] == 'P':
                     return 0
-                if 0 <= x-1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
+            
+            # 거리가 2인 곳에 응시자가 있는 경우 - 대각선
+            if 0 < x and y < 4:
+                if place[y+1][x-1] == 'P' and (place[y+1][x] == 'O' or place[y][x-1] == 'O'):
                     return 0
-            if 0 <= y-1 < 5 and place[y-1][x] == 'O':
-                if 0 <= y-2 < 5 and place[y-2][x] == 'P':
+            if x < 4 and 0 < y:
+                if place[y-1][x+1] == 'P' and (place[y-1][x] == 'O' or place[y][x+1] == 'O'):
                     return 0
-                if 0 <= x-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
+            if 0 < x and 0 < y:
+                if place[y-1][x-1] == 'P' and (place[y-1][x] == 'O' or place[y][x-1] == 'O'):
                     return 0
-                if 0 <= x+1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
-                    return 0
-            if 0 <= x+1 < 5 and place[y][x+1] == 'O':
-                if 0 <= x+2 < 5 and place[y][x+2] == 'P':
-                    return 0
-                if 0 <= y+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
-                    return 0
-                if 0 <= y-1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
-                    return 0
-            if 0 <= x-1 < 5 and place[y][x-1] == 'O':
-                if 0 <= x-2 < 5 and place[y][x-2] == 'P':
-                    return 0
-                if 0 <= y-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
-                    return 0
-                if 0 <= y+1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
-                    return 0
+            if x < 4 and y < 4:
+                if place[y+1][x+1] == 'P' and (place[y+1][x] == 'O' or place[y][x+1] == 'O'):
+                    return 0  
     return 1
+
 def solution(places):
     answer = []
     
     for place in places:
-        answer.append(getAnswer(place))
+        answer.append(isCorrect(place))
         
     return answer

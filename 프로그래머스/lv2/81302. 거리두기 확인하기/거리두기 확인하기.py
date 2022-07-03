ed42836b08486e03d0ab565f92c2efac5710ce75
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2022-07-03 10:44:33 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,69 +1,51 @@
+def getAnswer(place):
+    for y in range(5):
+        for x in range(5):
+            if place[y][x] == 'P':
+                # 거리가 1인 곳에 응시자가 있는 경우
+                if 0 <= y+1 < 5 and place[y+1][x] == 'P':
+                    return 0
+                if 0 <= y-1 < 5 and place[y-1][x] == 'P':
+                    return 0
+                if 0 <= x+1 < 5 and place[y][x+1] == 'P':
+                    return 0
+                if 0 <= x-1 < 5 and place[y][x-1] == 'P':
+                    return 0
+
+                # 거리가 2인 곳에 응시자가 있는 경우
+                if 0 <= y+1 < 5 and place[y+1][x] == 'O':
+                    if 0 <= y+2 < 5 and place[y+2][x] == 'P':
+                        return 0
+                    if 0 <= x+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
+                        return 0
+                    if 0 <= x-1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
+                        return 0
+                if 0 <= y-1 < 5 and place[y-1][x] == 'O':
+                    if 0 <= y-2 < 5 and place[y-2][x] == 'P':
+                        return 0
+                    if 0 <= x-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
+                        return 0
+                    if 0 <= x+1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
+                        return 0
+                if 0 <= x+1 < 5 and place[y][x+1] == 'O':
+                    if 0 <= x+2 < 5 and place[y][x+2] == 'P':
+                        return 0
+                    if 0 <= y+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
+                        return 0
+                    if 0 <= y-1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
+                        return 0
+                if 0 <= x-1 < 5 and place[y][x-1] == 'O':
+                    if 0 <= x-2 < 5 and place[y][x-2] == 'P':
+                        return 0
+                    if 0 <= y-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
+                        return 0
+                    if 0 <= y+1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
+                        return 0
+    return 1
 def solution(places):
     answer = []
     
     for place in places:
-        isPass = True
-        for y in range(5):
-            for x in range(5):
-                if place[y][x] == 'P':
-                    # 거리가 1인 곳에 응시자가 있는 경우
-                    if 0 <= y+1 < 5 and place[y+1][x] == 'P':
-                        isPass = False
-                        break
-                    if 0 <= y-1 < 5 and place[y-1][x] == 'P':
-                        isPass = False
-                        break
-                    if 0 <= x+1 < 5 and place[y][x+1] == 'P':
-                        isPass = False
-                        break
-                    if 0 <= x-1 < 5 and place[y][x-1] == 'P':
-                        isPass = False
-                        break
-                        
-                    # 거리가 2인 곳에 응시자가 있는 경우
-                    if 0 <= y+1 < 5 and place[y+1][x] == 'O':
-                        if 0 <= y+2 < 5 and place[y+2][x] == 'P':
-                            isPass = False
-                            break
-                        if 0 <= x+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                        if 0 <= x-1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                    if 0 <= y-1 < 5 and place[y-1][x] == 'O':
-                        if 0 <= y-2 < 5 and place[y-2][x] == 'P':
-                            isPass = False
-                            break
-                        if 0 <= x-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                        if 0 <= x+1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                    if 0 <= x+1 < 5 and place[y][x+1] == 'O':
-                        if 0 <= x+2 < 5 and place[y][x+2] == 'P':
-                            isPass = False
-                        if 0 <= y+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                        if 0 <= y-1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                    if 0 <= x-1 < 5 and place[y][x-1] == 'O':
-                        if 0 <= x-2 < 5 and place[y][x-2] == 'P':
-                            isPass = False
-                        if 0 <= y-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-                        if 0 <= y+1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
-                            isPass = False
-                            break
-            if not isPass:
-                break
-        if not isPass:
-            answer.append(0)
-        else:
-            answer.append(1)
+        answer.append(getAnswer(place))
         
     return answer

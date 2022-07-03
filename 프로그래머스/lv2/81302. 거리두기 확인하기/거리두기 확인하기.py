0d3d4a18f0f790da1215c6aa69fd7d405f7056c9
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2022-07-03 11:16:54 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,46 +1,44 @@
 def getAnswer(place):
     for y in range(5):
         for x in range(5):
-            if place[y][x] == 'P':
-                # 거리가 1인 곳에 응시자가 있는 경우
-                if 0 <= y+1 < 5 and place[y+1][x] == 'P':
-                    return 0
-                if 0 <= y-1 < 5 and place[y-1][x] == 'P':
-                    return 0
-                if 0 <= x+1 < 5 and place[y][x+1] == 'P':
-                    return 0
-                if 0 <= x-1 < 5 and place[y][x-1] == 'P':
-                    return 0
+            if place[y][x] != 'P':
+                continue
+                
+            # 거리가 1인 곳에 응시자가 있는 경우
+            if 0 < y < 4 and (place[y+1][x] == 'P' or place[y-1][x] == 'P'):
+                return 0
+            if 0 < x < 4 and (place[y][x+1] == 'P' or place[y][x-1] == 'P'):
+                return 0
 
-                # 거리가 2인 곳에 응시자가 있는 경우
-                if 0 <= y+1 < 5 and place[y+1][x] == 'O':
-                    if 0 <= y+2 < 5 and place[y+2][x] == 'P':
-                        return 0
-                    if 0 <= x+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
-                        return 0
-                    if 0 <= x-1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
-                        return 0
-                if 0 <= y-1 < 5 and place[y-1][x] == 'O':
-                    if 0 <= y-2 < 5 and place[y-2][x] == 'P':
-                        return 0
-                    if 0 <= x-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
-                        return 0
-                    if 0 <= x+1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
-                        return 0
-                if 0 <= x+1 < 5 and place[y][x+1] == 'O':
-                    if 0 <= x+2 < 5 and place[y][x+2] == 'P':
-                        return 0
-                    if 0 <= y+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
-                        return 0
-                    if 0 <= y-1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
-                        return 0
-                if 0 <= x-1 < 5 and place[y][x-1] == 'O':
-                    if 0 <= x-2 < 5 and place[y][x-2] == 'P':
-                        return 0
-                    if 0 <= y-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
-                        return 0
-                    if 0 <= y+1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
-                        return 0
+            # 거리가 2인 곳에 응시자가 있는 경우
+            if 0 <= y+1 < 5 and place[y+1][x] == 'O':
+                if 0 <= y+2 < 5 and place[y+2][x] == 'P':
+                    return 0
+                if 0 <= x+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
+                    return 0
+                if 0 <= x-1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
+                    return 0
+            if 0 <= y-1 < 5 and place[y-1][x] == 'O':
+                if 0 <= y-2 < 5 and place[y-2][x] == 'P':
+                    return 0
+                if 0 <= x-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
+                    return 0
+                if 0 <= x+1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
+                    return 0
+            if 0 <= x+1 < 5 and place[y][x+1] == 'O':
+                if 0 <= x+2 < 5 and place[y][x+2] == 'P':
+                    return 0
+                if 0 <= y+1 < 5 and place[y+1][x+1] == 'P': # 대각선의 경우
+                    return 0
+                if 0 <= y-1 < 5 and place[y-1][x+1] == 'P': # 대각선의 경우
+                    return 0
+            if 0 <= x-1 < 5 and place[y][x-1] == 'O':
+                if 0 <= x-2 < 5 and place[y][x-2] == 'P':
+                    return 0
+                if 0 <= y-1 < 5 and place[y-1][x-1] == 'P': # 대각선의 경우
+                    return 0
+                if 0 <= y+1 < 5 and place[y+1][x-1] == 'P': # 대각선의 경우
+                    return 0
     return 1
 def solution(places):
     answer = []

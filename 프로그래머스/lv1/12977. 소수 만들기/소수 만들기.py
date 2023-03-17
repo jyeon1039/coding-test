2e6825eb0d73b94fe89308747b9b814fb6087ec8
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-03-17 12:01:59 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+import math
+
+# 소수인지를 판별하는 함수
+def isPrimeNumber(x):
+    for i in range (2, int(math.sqrt(x) + 1)):
+    	if x % i == 0:
+        	return False
+    return True	 
+
+def solution(nums):
+    answer = 0
+    combi = list(combinations(nums, 3))
+
+    for x in combi:
+        if isPrimeNumber(sum(x)):
+            answer += 1
+                    
+    return answer

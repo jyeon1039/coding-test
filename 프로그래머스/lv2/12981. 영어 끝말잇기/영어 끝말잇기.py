f854059580bd7a437f2e6f81ae618f60c4199d51
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-04-19 15:35:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,11 +1,18 @@
-def solution(n, words):    
+def solution(n, words):
+    humanN = 2 # 몇 번 사람인지 저장하는 인덱스
+    idx = 1 # 몇 번째 차례인지 저장하는 인덱스
+
     for i in range(1, len(words)):
-        w = words[i] # 현재 문자
-        e = words[i-1][-1] # 이전 차례 사람의 끝 문자
-        
-        # 끝 문자와 시작하는 문자가 다를 경우
-        # 이전에 등장했던 문자인 경우
+        w = words[i]
+        e = words[i-1][-1] # 전 사람의 끝 문자
+
         if (w[0] != e) or (w in words[:i-1]):
-            return [i%n+1, i//n+1]
-    
-    return [0, 0] # 탈락자가 발생하지 않는 경우
+            return [humanN, idx]
+
+        if humanN == n:
+            humanN = 1
+            idx += 1 # 한 바퀴 돌았으니까 +1
+        else:
+            humanN += 1
+    else:
+        return [0, 0]

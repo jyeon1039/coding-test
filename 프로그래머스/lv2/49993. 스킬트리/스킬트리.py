d47a50a25283c43869f1049d14a68483a1b35fce
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-04-26 11:31:11 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,13 +1,13 @@
 def solution(skill, skill_trees):
     answer = 0
-    
+
     for tree in skill_trees:
         s = ''
         for c in tree:
             if c in skill:
                 s += c
-        
+
         if s == skill[:len(s)]:
             answer += 1
-        
+
     return answer

e0633fb50b0141e96c8ac6c80bbd426acbc94d5b
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-04-29 14:47:17 +0900

    [Silver V] Title: 폴리오미노, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,20 +1,9 @@
-s = input().split('.')
+s = input()
 
-for i in range(len(s)):
-    x = list(s[i])
-    n = len(x)
-    if n % 2 != 0: #홀수일 경우에는 불가능
-        print(-1)
-        break
-    else:
-        if n == 2: # 2개인 경우
-            x = 'BB'
-        else: # 4개를 넘어가는 경우
-            for j in range(0, 4 * (n//4)):
-                x[j] = 'A'
-            if n % 4 != 0: # 2개가 더 남은 경우
-                x[-1] = 'B'
-                x[-2] = 'B'
-        s[i] = ''.join(x)
+s = s.replace('XXXX', 'AAAA')
+s = s.replace('XX', 'BB')
+
+if s.count('X') != 0:
+    print(-1)
 else:
-    print('.'.join(s))
+    print(s)

229de4efdd1d0cc6d94c623edeb264a68307c556
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-05-16 12:39:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 708 ms, Memory: 65152 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -2,27 +2,25 @@ import sys
 
 sys.setrecursionlimit(10000)
 
-n, m = map(int, sys.stdin.readline().split())
-graph = [[0]*(n+1) for i in range(n+1)]
-visited = [False]*(n+1)
-
 def dfs(v):
-    visited[v] = True
-    
-    for u in range(1, n+1):
-        if graph[v][u] == 1:
-            if not visited[u]:
-                dfs(u)
-    
-for i in range(m):
-    x, y = map(int, sys.stdin.readline().split())
-    graph[y][x] = 1
-    graph[x][y] = 1
-
+    for u in graph[v]:
+        if visited[u] == False:
+            visited[u] = True
+            dfs(u)
+                
+n, m = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 res = 0
-for i in range(1, n+1):
-    if not visited[i]:
-        dfs(i)
+
+for _ in range(m):
+    v, u = map(int, sys.stdin.readline().split())
+    graph[v].append(u)
+    graph[u].append(v)
+
+for v in range(1, n+1):
+    if visited[v] == False:
+        dfs(v)
         res += 1
 
 print(res)

2306fb84c3e22ce4672a5340c6f749cd5d0b0b8b
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-06-13 10:28:55 +0900

    [level 3] Title: 숫자 게임, Time: 45.10 ms, Memory: 18.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,13 @@
+def solution(A, B):
+    answer = 0
+    j = 0
+    
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    
+    for i in range(len(A)):
+        if A[i] < B[j]:
+            answer += 1
+            j += 1
+            
+    return answer

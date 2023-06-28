7fdca1c7d13e30e809f7e4284938503ab1efc9b7
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-06-28 13:17:10 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -10,35 +10,26 @@ for _ in range(n):
     arr.append(list(map(int, input().split())))
 
 while True:
-    if arr[r][c] == 0: # 현재 칸이 0인 경우(청소되지 않은 칸인 경우)
+    if arr[r][c] == 0: # 현재 칸이 청소되지 않은 칸인 경우
         arr[r][c] = 2
         result += 1
     elif arr[r][c] == 1: # 현재 칸이 벽인 경우 작동 멈춤
         break
 
-    # 주변 4칸 중 청소해야 하는 빈칸 있는지 탐색
-    visit = [0]*4
-    idx = 0
-    for (dr, dc) in go_dirs:
-        nr = r + dr
-        nc = c + dc
+    idx = d
+    for i in range(1, 5): # 주변 4칸 중 청소되지 않은 빈칸이 있는 경우
+        idx -= 1
+        
+        if idx < 0:
+            idx = 3
+
+        nr = r + go_dirs[idx][0]
+        nc = c + go_dirs[idx][1]
 
         if 0 <= nr < n and 0 <= nc < m and arr[nr][nc] == 0:
-            visit[idx] = 1
-
-        idx += 1
-
-    tmp = d
-    for i in range(1, 5): # 주변 4칸 중 청소되지 않은 빈칸이 있는 경우
-        tmp -= 1
-
-        if tmp < 0:
-            tmp = 3
-            
-        if visit[tmp]:
-            d = tmp
-            r += go_dirs[d][0]
-            c += go_dirs[d][1]
+            d = idx
+            r  = nr
+            c = nc
             break
     else: # 청소되지 않은 빈칸이 없는 경우 후진
         r += back_dirs[d][0]

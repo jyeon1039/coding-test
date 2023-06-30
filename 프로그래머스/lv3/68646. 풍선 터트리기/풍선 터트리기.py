774b61d94cefb90b34e9d025a3b116b246a21447
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-06-30 20:45:08 +0900

    [level 3] Title: 풍선 터트리기, Time: 421.64 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -2,8 +2,7 @@ def solution(a):
     answer = [False]*len(a) # 양 쪽은 끝까지 살아남을 수 있음
     lmin = 1000000001
     rmin = 1000000001
-    
-    # 양쪽의 최소값 중 하나라도 자신보다 크면 됨. 즉 자신이 양쪽 중에서 최소값이면 됨
+
     for i in range(len(a)):
         if a[i] < lmin:
             answer[i] = True
@@ -11,5 +10,5 @@ def solution(a):
         if a[(len(a)-1)-i] < rmin:
             answer[(len(a)-1)-i] = True
             rmin = a[(len(a)-1)-i]
-                
+
     return sum(answer)

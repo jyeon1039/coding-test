b5e638ac6c323b0e89bf25bf3ac6f2b6cdc9e7b3
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-08-29 19:13:15 +0900

    [level 2] Title: 튜플, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,15 +1,13 @@
 def solution(s):
     answer = []
+    s = s.lstrip("{").rstrip("}").split("},{")
+    s.sort(key=len)
     
-    s = s.lstrip('{').rstrip('}').split('},{')
-    arr = []
     for x in s:
-        arr.append(list(map(int, x.split(","))))
-        
-    arr.sort(key=len)
-    for x in arr:
-        for y in x:
-            if y not in answer:
-                answer.append(y)
+        num = x.split(",")
+        for n in num:
+            n = int(n)
+            if n not in answer:
+                answer.append(n)
     
     return answer

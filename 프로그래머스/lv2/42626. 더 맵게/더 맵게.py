877345a872098f778763007e0e8cc5575cc8b620
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-09-06 10:54:37 +0900

    [level 2] Title: 더 맵게, Time: 1943.38 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -4,17 +4,16 @@ def solution(scoville, K):
     answer = 0
     heap = []
     
-    for x in scoville:
-        heapq.heappush(heap, x)
-    
+    for s in scoville:
+        heapq.heappush(heap, s)
+        
     while heap[0] < K:
         answer += 1
-        x = heapq.heappop(heap)
-        y = heapq.heappop(heap)
-        heapq.heappush(heap, x + 2*y)
+        s1 = heapq.heappop(heap)
+        s2 = heapq.heappop(heap)
+        heapq.heappush(heap, s1 + s2*2)
         
-        if len(heap) == 1 and heap[0] < K: # 모든 음식의 스코빌 지수가 K 이상이 아닐 때
-            answer = -1
-            break
+        if len(heap) == 1 and heap[0] < K:
+            return -1
         
     return answer

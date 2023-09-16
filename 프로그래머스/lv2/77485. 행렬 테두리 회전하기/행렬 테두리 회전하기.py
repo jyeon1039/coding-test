045e1aea771623d5b6c15becd491e18d89af56be
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-09-16 15:06:37 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 126.47 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -0,0 +1,45 @@
+def solution(rows, columns, queries):
+    answer = []
+    arr = [[0 for _ in range(columns+1)] for _ in range(rows+1)]
+
+    num = 1
+    # 행렬 생성
+    for y in range(1, rows+1):
+        for x in range(1, columns+1):
+            arr[y][x] = num
+            num += 1
+            
+    if rows == 4:
+        print(arr)
+    for y1, x1, y2, x2 in queries:
+        tmp = arr[y1][x1]
+        mini = tmp
+        
+        # 왼쪽 세로
+        for y in range(y1, y2):
+            test = arr[y+1][x1]
+            arr[y][x1] = test
+            mini = min(mini, test)
+
+        # 하단 가로
+        for x in range(x1, x2):
+            test = arr[y2][x+1]
+            arr[y2][x] = test
+            mini = min(mini, test)
+
+        # 오른쪽 세로
+        for y in range(y2, y1, -1):
+            test = arr[y-1][x2]
+            arr[y][x2] = test
+            mini = min(mini, test)
+
+        # 상단 가로
+        for x in range(x2, x1, -1):
+            test = arr[y1][x-1]
+            arr[y1][x] = test
+            mini = min(mini, test)
+
+        arr[y1][x1+1] = tmp
+        answer.append(mini)
+            
+    return answer

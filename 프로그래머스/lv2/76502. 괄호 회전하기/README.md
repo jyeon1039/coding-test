698badb8520491f5932451941d89ffa8c7be9874
jihyeon <69183944+jyeon1039@users.noreply.github.com>
2023-10-01 18:04:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.43 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/lv2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.28 ms
+메모리: 74.6 MB, 시간: 1.43 ms
 
 ### 구분
 
--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -32,17 +32,11 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         StringBuilder sb = new StringBuilder();
-        answer += isValid(s);
-        sb.append(s.substring(1));
-        sb.append(s.charAt(0));
         
         // 왼쪽으로 회전
-        while (!sb.toString().equals(s)){
-            StringBuilder tmp = new StringBuilder();
-            answer += isValid(sb.toString());
-            tmp.append(sb.toString().substring(1));
-            tmp.append(sb.toString().charAt(0));
-            sb = tmp;
+        for(int i=0; i<s.length(); i++){
+            String tmp = s.substring(i, s.length()) + s.substring(0, i);
+            answer += isValid(tmp);
         }
         
         return answer;
